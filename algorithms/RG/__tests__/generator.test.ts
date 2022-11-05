4072b14f8d4f85506949c70399585736ce099e08
lucasgdb <lucasgdbittencourt@gmail.com>
2022-11-05 13:21:39 -0300

    chore: remove unused dependency

--- algorithms/RG/__tests__/generator.test.ts
+++ algorithms/RG/__tests__/generator.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+
 import { generateRG } from '../generator';
 import { validateRG } from '../validator';
 
--- algorithms/RG/utils/__tests__/getCheckerTypist.test.ts
+++ algorithms/RG/utils/__tests__/getCheckerTypist.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+
 import { getCheckerTypist } from '../getCheckerTypist';
 
 describe('getCheckerTypist', () => {
